5d46452266eb4aabbc8b603cd6d0491e5576847a
g3offeryy <105028190+g3offeryy@users.noreply.github.com>
2022-05-06 10:02:11 +0800

    Delete nav.php

--- nav.php
+++
@@ -1,8 +0,0 @@
-<h1>Police Emergency Service System</h1>
-<nav id="HeaderStyle">
-
-   <a href="logcall.php">Log Call</a>
-    <a href="Update.php">Update</a>
-    <a href="#">Report</a>
-   <a href="#">History</a>
-  </nav>
