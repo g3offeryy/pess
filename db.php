4aa2dcaebdd5a0b098edae545c8a53acd80111b1
g3offeryy <105028190+g3offeryy@users.noreply.github.com>
2022-05-11 11:50:19 +0800

    Delete db.php

--- db.php
+++
@@ -1,6 +0,0 @@
-<?php // define database connection variables as constant
-define('DB_USER', "root"); // db user
-define('DB_PASSWORD', ""); // db password 
-define('DB_DATABASE', "pessdb"); // database name
-define('DB_SERVER', "localhost"); // db server
-?>
