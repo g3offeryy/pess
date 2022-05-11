129b70d0618f0980e7c0345bdbbad0c43705239c
g3offeryy <105028190+g3offeryy@users.noreply.github.com>
2022-05-11 11:49:09 +0800

    Add files via upload

---
+++ geoffrey_pessdb.sql
@@ -0,0 +1,209 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 11, 2022 at 04:46 AM
+-- Server version: 10.4.20-MariaDB
+-- PHP Version: 8.0.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pessdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `dispatch`
+--
+
+CREATE TABLE `dispatch` (
+  `incident_id` int(11) NOT NULL,
+  `patrolcar_id` varchar(10) NOT NULL,
+  `time_dispatched` datetime NOT NULL,
+  `time_arrival` datetime DEFAULT NULL,
+  `time_complete` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `dispatch`
+--
+
+INSERT INTO `dispatch` (`incident_id`, `patrolcar_id`, `time_dispatched`, `time_arrival`, `time_complete`) VALUES
+(12, 'QX2222G', '2022-03-02 05:05:25', '2022-03-02 05:05:25', '2022-03-02 05:05:25'),
+(13, 'QX2222G', '2022-03-02 05:05:25', '2022-03-02 05:05:25', '2022-03-02 05:05:25'),
+(14, 'QX4444P', '2022-03-02 05:05:25', '2022-03-02 05:05:25', '2022-03-02 05:05:25');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `incident`
+--
+
+CREATE TABLE `incident` (
+  `incident_id` int(11) NOT NULL,
+  `caller_name` varchar(30) NOT NULL,
+  `phone_number` varchar(10) NOT NULL,
+  `incident_type_id` varchar(3) NOT NULL,
+  `incident_location` varchar(50) NOT NULL,
+  `incident_desc` varchar(100) NOT NULL,
+  `incident_status_id` varchar(1) NOT NULL,
+  `times_called` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `incident`
+--
+
+INSERT INTO `incident` (`incident_id`, `caller_name`, `phone_number`, `incident_type_id`, `incident_location`, `incident_desc`, `incident_status_id`, `times_called`) VALUES
+(6, 'PETER LEOW', '81234567', '010', 'Junction of North Bridge Rd and middle Rd', 'a bus collided with two taxi 2 injuires', '1', '2022-03-02 13:07:00'),
+(8, 'qwerty', '12345678', '030', 'cck', 'qwerty', '3', '2022-03-02 13:07:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `incident_status`
+--
+
+CREATE TABLE `incident_status` (
+  `incident_status_id` varchar(1) NOT NULL,
+  `iden_status_desc` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `incident_status`
+--
+
+INSERT INTO `incident_status` (`incident_status_id`, `iden_status_desc`) VALUES
+('1', 'Pending'),
+('2', 'Dispatched'),
+('3', 'Complete'),
+('4', 'Duplicate');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `incident_type`
+--
+
+CREATE TABLE `incident_type` (
+  `incident_type_id` varchar(3) NOT NULL,
+  `incident_type_desc` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `incident_type`
+--
+
+INSERT INTO `incident_type` (`incident_type_id`, `incident_type_desc`) VALUES
+('010', 'Fire'),
+('020', 'Riot'),
+('030', 'Burglary'),
+('040', 'Domestic violence'),
+('050', 'Fallen tree'),
+('060', 'Traffic accident'),
+('070', 'Loan shark'),
+('999', 'others');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `patrolcar`
+--
+
+CREATE TABLE `patrolcar` (
+  `patrolcar_id` varchar(10) NOT NULL,
+  `patrolcar_status_id` varchar(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `patrolcar`
+--
+
+INSERT INTO `patrolcar` (`patrolcar_id`, `patrolcar_status_id`) VALUES
+('QX1234', '3'),
+('QX4321R', '3'),
+('QX4444P', '1'),
+('QX45545', '1'),
+('QX9999H', '1');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `patrolcar_status`
+--
+
+CREATE TABLE `patrolcar_status` (
+  `patrolcar_status_id` varchar(1) NOT NULL,
+  `patrolcar_status_desc` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `patrolcar_status`
+--
+
+INSERT INTO `patrolcar_status` (`patrolcar_status_id`, `patrolcar_status_desc`) VALUES
+('1', 'Dispatched'),
+('2', 'Patrol'),
+('3', 'Free'),
+('4', 'On-site'),
+('5', 'NA');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `dispatch`
+--
+ALTER TABLE `dispatch`
+  ADD PRIMARY KEY (`incident_id`,`patrolcar_id`);
+
+--
+-- Indexes for table `incident`
+--
+ALTER TABLE `incident`
+  ADD PRIMARY KEY (`incident_id`);
+
+--
+-- Indexes for table `incident_status`
+--
+ALTER TABLE `incident_status`
+  ADD PRIMARY KEY (`incident_status_id`);
+
+--
+-- Indexes for table `patrolcar`
+--
+ALTER TABLE `patrolcar`
+  ADD PRIMARY KEY (`patrolcar_id`);
+
+--
+-- Indexes for table `patrolcar_status`
+--
+ALTER TABLE `patrolcar_status`
+  ADD PRIMARY KEY (`patrolcar_status_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `incident`
+--
+ALTER TABLE `incident`
+  MODIFY `incident_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
